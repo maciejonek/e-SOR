b486419a04a95ff6f3a7a1ae48d072f5ed80d436
Maciej Filipionek <maciejfilipionek@icloud.com>
2025-01-06 13:38:57 +0100

    Add sample data extractor for testing

---
+++ get_test_data.py
@@ -0,0 +1,26 @@
+import pandas as pd
+
+### Get sample for request operation to model API
+
+df = pd.read_csv('new_data.csv')
+df.reset_index(drop=True, inplace=True)
+df.head()
+
+df['Row_Sum'] = df.drop(columns=['esi', 'age', 'n_surgeries']).sum(axis=1)
+
+#filtered_df = df[df['Row_Sum'] == 4]
+filtered_df = df[df['esi'] == 2]
+
+random_sample = filtered_df.sample(n=1)
+
+selected_columns = ['esi', 'age', 'n_surgeries', 'Row_Sum'] + [col for col in random_sample.columns if random_sample[col].eq(1).any() and col not in ['esi', 'age', 'n_surgeries', 'Row_Sum']]
+
+random_sample = random_sample[selected_columns]
+
+random_sample = random_sample.drop(columns=['Row_Sum'])
+
+json_data = random_sample.to_dict(orient='records')[0]
+
+import json
+with open('sample.json', 'w') as json_file:
+    json.dump(json_data, json_file)
